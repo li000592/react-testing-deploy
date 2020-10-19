4c0fe0fcb56c4ed2050706ca4ca969ea7cc95359
“Haorong <li000592@algonquinlive.com>
2020-10-19 18:45:25 -0400

    Updates

---
+++ precache-manifest.b4198a7ec3aeefc960832f9f95fe93a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d7994a5d1a2eaf180a98be3d58d535",
+    "url": "/li000592/react-testing-deploy/index.html"
+  },
+  {
+    "revision": "0748d319c8861d1f644c",
+    "url": "/li000592/react-testing-deploy/static/css/main.18a26f74.chunk.css"
+  },
+  {
+    "revision": "553519c07da55fa8dba0",
+    "url": "/li000592/react-testing-deploy/static/js/2.3dd7f40c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/li000592/react-testing-deploy/static/js/2.3dd7f40c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0748d319c8861d1f644c",
+    "url": "/li000592/react-testing-deploy/static/js/main.24aa87f5.chunk.js"
+  },
+  {
+    "revision": "256b203bb902e14acd45",
+    "url": "/li000592/react-testing-deploy/static/js/runtime-main.af055f33.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/li000592/react-testing-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
